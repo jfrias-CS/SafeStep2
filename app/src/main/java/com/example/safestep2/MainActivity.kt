945c4af32ddaa6b8e8103ba05c5c45f1d3af0520
Jay Code's <jfrias9@horizon.csueastbay.edu>
2025-05-07 16:08:57 -0700

    Your new commit message here

--- app/src/main/java/com/example/safestep2/MainActivity.kt
+++ app/src/main/java/com/example/safestep2/MainActivity.kt
@@ -108,7 +108,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
     //region Constants
     companion object {
         private const val CAMERA_PERMISSION_REQUEST = 200
-        private const val OPENAI_API_KEY = "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"
+        private const val OPENAI_API_KEY = "YOUR_API_KEY"
         private const val OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
         private const val FINE_TUNED_MODEL_ID = "ft:gpt-3.5-turbo-0125:ilab::BGHKK6va"
     }
