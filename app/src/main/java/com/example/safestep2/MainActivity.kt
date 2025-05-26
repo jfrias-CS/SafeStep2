f458bb54c544aa9af0e55f47977fd13869e86e09
Jay Code's <jfrias9@horizon.csueastbay.edu>
2025-05-25 17:45:18 -0700

    Updated APP functionalities: Diagnostic mode for individual steps of app process, Auto Run for app processes, Updated UX/UI

--- app/src/main/java/com/example/safestep2/MainActivity.kt
+++ app/src/main/java/com/example/safestep2/MainActivity.kt
@@ -106,12 +106,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
 
 
     //region Constants
-    companion object {
-        private const val CAMERA_PERMISSION_REQUEST = 200
-        private const val OPENAI_API_KEY = "REPLACE_HERE"
-        private const val OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
-        private const val FINE_TUNED_MODEL_ID = "ft:gpt-3.5-turbo-0125:ilab::BGHKK6va"
-    }
+
     //endregion
 
     //region Activity Lifecycle
