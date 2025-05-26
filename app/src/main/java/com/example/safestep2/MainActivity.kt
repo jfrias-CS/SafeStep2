b1e174a6e8281c8b682465505e31becdcc617963
Jay Code's <jfrias9@horizon.csueastbay.edu>
2025-05-25 18:16:18 -0700

    Removed secret scanning

--- app/src/main/java/com/example/safestep2/MainActivity.kt
+++ app/src/main/java/com/example/safestep2/MainActivity.kt
@@ -106,7 +106,12 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback, Camera.Picture
 
 
     //region Constants
-
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST = 200
+        private const val OPENAI_API_KEY = "PLACE_HOLDER"
+        private const val OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
+        private const val FINE_TUNED_MODEL_ID = "ft:gpt-3.5-turbo-0125:ilab::BGHKK6va"
+    }
     //endregion
 
     //region Activity Lifecycle
